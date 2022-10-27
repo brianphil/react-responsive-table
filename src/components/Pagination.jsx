6395c7206c1cdea982b71aa9820024d64194d249
brianphil <brianphilemon@gmail.com>
2022-10-27 11:31:38 +0300

    styled pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Pagination = ({
   dataPerPage,
@@ -11,20 +11,29 @@ const Pagination = ({
   for (let i = 1; i <= Math.ceil(totalData / dataPerPage); i++) {
     pageNumbers.push(i);
   }
-
+  const [isActivePage, setisActivePage] = useState(1);
   const navigatePage = (param) => {
     switch (param) {
       case 1: {
         if (currentPage === 1) return;
-        else setcurrentPage((prev) => prev - 1);
+        else {
+          setcurrentPage((prev) => prev - 1);
+          setisActivePage(currentPage - 1);
+        }
       }
       case 2: {
         if (currentPage === pageNumbers.length) return;
         else {
           setcurrentPage((prev) => prev + 1);
+          setisActivePage(currentPage + 1);
         }
       }
     }
+  };
+
+  const handleClick = (page) => {
+    selectedPage(page);
+    setisActivePage(page);
   };
 
   return (
@@ -35,15 +44,17 @@ const Pagination = ({
       {pageNumbers.map((page) => {
         return (
           <li
-            onClick={() => selectedPage(page)}
+            onClick={() => handleClick(page)}
             key={page}
-            className="pagination"
+            className={
+              isActivePage == page ? "pagination active-page" : "pagination"
+            }
           >
             {page}
           </li>
         );
       })}
-      <li onClick={()=>navigatePage(2)} className="pagination">
+      <li onClick={() => navigatePage(2)} className="pagination">
         {"Next>>"}
       </li>
     </ul>
